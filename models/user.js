1135df91829988eb3f7010575d27e60009b7ba27
aaloksikand <aalok.sikand@gmail.com>
2022-08-01 19:17:10 -0700

    added relevant code from class activity 8

--- models/user.js
+++ models/user.js
@@ -74,4 +74,61 @@ const data = [
   },
 ];
 
+// Data for document
+const data = [
+  {
+    title: 'Good Omens',
+    // One-to-many relationship
+    // Each book has multiple authors
+    authors: [
+      { name: 'Neil Gaiman', featured: true },
+      { name: 'Terry Pratchett', featured: true },
+    ],
+    information: { ISBN: 9780425132159, price: 10, total_in_stock: 10 },
+  },
+  {
+    title: 'Heads You Lose',
+    authors: [
+      { name: 'Lisa Lutz', featured: false },
+      { name: 'David Hayward', featured: false },
+    ],
+    // One-to-one embedded document relationship
+    // Each book has one set of information
+    information: { ISBN: 9780399157400, price: 20, total_in_stock: 8 },
+  },
+  {
+    title: 'Between the Lines',
+    authors: [
+      { name: 'Jodi Picoult', featured: true },
+      { name: 'Samantha Van Leer', featured: false },
+    ],
+    information: { ISBN: 9781451635751, price: 5, total_in_stock: 5 },
+  },
+];
+
+app.use(express.json());
+
+// Accessing embedded document with one-to-one relationship
+app.get('/price-less-than-10', (req, res) => {
+  db.collection('authorList')
+    // Use dot notation to query on an embedded document
+    .find({ 'information.price': { $lt: 10 } })
+    .toArray((err, results) => {
+      if (err) throw err;
+      res.send(results);
+    });
+});
+
+// Accessing embedded document with one-to-many relationship
+app.get('/featured-authors', (req, res) => {
+  db.collection('authorList')
+    // If you do not know the array index, use dot notation to access fields nested in arrays
+    .find({ 'authors.featured': true })
+    .toArray((err, results) => {
+      if (err) throw err;
+      res.send(results);
+    });
+});
+
+
 module.exports = data;
