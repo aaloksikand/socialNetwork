ae406ae636c847d5a528d16e4043446315c1e559
aaloksikand <aalok.sikand@gmail.com>
2022-08-01 18:47:14 -0700

    adding relevant code from class activity 4

--- server.js
+++ server.js
@@ -48,3 +48,85 @@ app.get('/read', (req, res) => {
       res.send(results);
     });
 });
+// Post request to create a single document to collection
+app.post('/create', (req, res) => {
+  // collection() creates or selects instance of collection. Takes in collection name
+  // insertOne() inserts single document into collection. Takes in object.
+  db.collection('bookCollection').insertOne(
+    { title: req.body.title, author: req.body.author },
+    // Handles error or results
+    (err, results) => {
+      if (err) throw err;
+      res.json(results);
+    }
+  );
+});
+
+// Post request to add multiple document to collection
+app.post('/create-many', function (req, res) {
+  db.collection('bookCollection').insertMany(
+    [
+      {"title" : "Oh the Places We Will Go!"},
+      {"title" : "Diary of Anne Frank"}
+    ], 
+    (err,results) => {
+      if (err) throw err;
+      res.json(results);
+    }
+  );
+});
+
+// Get request to read all the documents in a collection
+app.get('/read', (req, res) => {
+  db.collection('bookCollection')
+    // find() returns all documents. Equivalent to `Select *` in SQL.
+    .find({})
+    // Returns all the documents in an array
+    .toArray((err, results) => {
+      // Handles error or results
+      if (err) throw err;
+      res.send(results);
+    });
+});
+
+// Post request to create a single document to collection
+app.post('/create', (req, res) => {
+  // collection() creates or selects instance of collection. Takes in collection name
+  // insertOne() inserts single document into collection. Takes in object.
+  db.collection('bookCollection').insertOne(
+    { title: req.body.title, author: req.body.author },
+    // Handles error or results
+    (err, results) => {
+      if (err) throw err;
+      res.json(results);
+    }
+  );
+});
+
+// Post request to add multiple document to collection
+app.post('/create-many', function (req, res) {
+  db.collection('bookCollection').insertMany(
+    [
+      {"title" : "Oh the Places We Will Go!"},
+      {"title" : "Diary of Anne Frank"}
+    ], 
+    (err,results) => {
+      if (err) throw err;
+      res.json(results);
+    }
+  );
+});
+
+// Get request to read all the documents in a collection
+app.get('/read', (req, res) => {
+  db.collection('bookCollection')
+    // find() returns all documents. Equivalent to `Select *` in SQL.
+    .find({})
+    // Returns all the documents in an array
+    .toArray((err, results) => {
+      // Handles error or results
+      if (err) throw err;
+      res.send(results);
+    });
+});
+
