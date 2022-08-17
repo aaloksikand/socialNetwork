5971c0a89083c76577c9ebb24b1cb96ee1b36935
aaloksikand <aalok.sikand@gmail.com>
2022-08-16 22:25:40 -0700

    cleaned up thoughtController.js

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,255 +1,100 @@
 const { Thought, User } = require('../models');
 
-module.exports = {
-  getThoughts(req, res) {
-    Thought.find()
-      .then((Thoughts) => res.json(Thoughts))
-      .catch((err) => res.status(500).json(err));
-  },
-  getSingleThought(req, res) {
-    Thought.findOne({ _id: req.params.ThoughtId })
-      .then((Thought) =>
-        !Thought
-          ? res.status(404).json({ message: 'No Thought with that ID' })
-          : res.json(Thought)
-      )
-      .catch((err) => res.status(500).json(err));
-  },
-  // create a new Thought
-  createThought(req, res) {
-    Thought.create(req.body)
-      .then((Thought) => {
-        return User.findOneAndUpdate(
-          { _id: req.body.userId },
-          { $addToSet: { Thoughts: Thought._id } },
-          { new: true }
-        );
-      })
-      .then((user) =>
-        !user
-          ? res
-              .status(404)
-              .json({ message: 'Thought created, but found no user with that ID' })
-          : res.json('Created the Thought 🎉')
-      )
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  },
-};
-const { Video, User } = require('../models');
+const thoughtController = {
 
-module.exports = {
-  getVideos(req, res) {
-    Video.find()
-      .then((videos) => res.json(videos))
-      .catch((err) => res.status(500).json(err));
-  },
-  getSingleVideo(req, res) {
-    Video.findOne({ _id: req.params.videoId })
-      .then((video) =>
-        !video
-          ? res.status(404).json({ message: 'No video with that ID' })
-          : res.json(video)
-      )
-      .catch((err) => res.status(500).json(err));
-  },
-  // create a new video
-  createVideo(req, res) {
-    Video.create(req.body)
-      .then((video) => {
-        return User.findOneAndUpdate(
-          { _id: req.body.userId },
-          { $addToSet: { videos: video._id } },
-          { new: true }
-        );
-      })
-      .then((user) =>
-        !user
-          ? res.status(404).json({
-              message: 'Video created, but found no user with that ID',
+    getThoughts(req, res) {
+        Thought.find({})
+        .populate({path: 'reactions', select: '-__v'})
+        .select('-__v')
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+    getOneThought(req, res) {
+        Thought.findOne({ _id: req.params.id })
+        .populate({ path: 'reactions', select: '-__v' })
+        .select('-__v')
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({message: 'No thought found'});
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+    createThought(req, res){
+        Thought.create(req.body)
+        .then(({ _id }) => {
+            return User.findOneAndUpdate({ _id: req.params.userId},
+                {$push: { thoughts: _id }}, 
+                {new: true});
+        })
+        .then(dbThoughtData => {
+            if(!dbThoughtData){
+                res.status(404).json({message: 'Error'});
+                return;
+            }
+            res.json(dbThoughtData)
+        })
+        .catch(err => res.json(err));
+    },
+    updateThought(req, res){
+        Thought.findOneAndUpdate({_id: req.params.id}, req.body, {new: true, runValidators: true})
+        .populate({path: 'reactions', select: '-__v'})
+        .select('-___v')
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({message: 'No thoughts found'});
+                return;
+            }
+                res.json(dbThoughtData);
+        })
+        .catch(err => res.json(err));
+    },
+    deleteThought(req, res){
+        Thought.findOneAndDelete({_id: req.params.id})
+        .then(dbThoughtData => {
+            if (!dbThoughtsData) {
+                res.status(404).json({message: 'No thoughts found'});
+                return;
+            }
+            res.json(dbThoughtData);
             })
-          : res.json('Created the video 🎉')
-      )
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  },
-  updateVideo(req, res) {
-    Video.findOneAndUpdate(
-      { _id: req.params.videoId },
-      { $set: req.body },
-      { runValidators: true, new: true }
-    )
-      .then((video) =>
-        !video
-          ? res.status(404).json({ message: 'No video with this id!' })
-          : res.json(video)
-      )
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  },
-  deleteVideo(req, res) {
-    Video.findOneAndRemove({ _id: req.params.videoId })
-      .then((video) =>
-        !video
-          ? res.status(404).json({ message: 'No video with this id!' })
-          : User.findOneAndUpdate(
-              { videos: req.params.videoId },
-              { $pull: { videos: req.params.videoId } },
-              { new: true }
-            )
-      )
-      .then((user) =>
-        !user
-          ? res
-              .status(404)
-              .json({ message: 'Video created but no user with this id!' })
-          : res.json({ message: 'Video successfully deleted!' })
-      )
-      .catch((err) => res.status(500).json(err));
-  },
-  // Add a video response
-  addVideoResponse(req, res) {
-    Video.findOneAndUpdate(
-      { _id: req.params.videoId },
-      { $addToSet: { responses: req.body } },
-      { runValidators: true, new: true }
-    )
-      .then((video) =>
-        !video
-          ? res.status(404).json({ message: 'No video with this id!' })
-          : res.json(video)
-      )
-      .catch((err) => res.status(500).json(err));
-  },
-  // Remove video response
-  removeVideoResponse(req, res) {
-    Video.findOneAndUpdate(
-      { _id: req.params.videoId },
-      { $pull: { reactions: { responseId: req.params.responseId } } },
-      { runValidators: true, new: true }
-    )
-      .then((video) =>
-        !video
-          ? res.status(404).json({ message: 'No video with this id!' })
-          : res.json(video)
-      )
-      .catch((err) => res.status(500).json(err));
-  },
-};
-const { Application, User } = require('../models');
+            .catch(err => res.status(400).json(err));
+    },
+    addReaction(req, res) {
+        Thought.findOneAndUpdate({_id: req.params.thoughtId}, {$push: {reactions: req.body}}, {new: true, runValidators: true})
+        .populate({path: 'reactions', select: '-__v'})
+        .select('-__v')
+        .then(dbThoughtData => {
+        if (!dbThoughtData) {
+            res.status(404).json({message: 'No thoughts found'});
+            return;
+        }
+        res.json(dbThoughtData);
+        })
+        .catch(err => res.status(400).json(err))
 
-module.exports = {
-  // Function to get all of the applications by invoking the find() method with no arguments.
-  // Then we return the results as JSON, and catch any errors. Errors are sent as JSON with a message and a 500 status code
-  getApplications(req, res) {
-    Application.find()
-      .then((applications) => res.json(applications))
-      .catch((err) => res.status(500).json(err));
-  },
-  // Gets a single application using the findOneAndUpdate method. We pass in the ID of the application and then respond with it, or an error if not found
-  getSingleApplication(req, res) {
-    Application.findOne({ _id: req.params.applicationId })
-      .then((application) =>
-        !application
-          ? res.status(404).json({ message: 'No application with that ID' })
-          : res.json(application)
-      )
-      .catch((err) => res.status(500).json(err));
-  },
-  // Creates a new application. Accepts a request body with the entire Application object.
-  // Because applications are associated with Users, we then update the User who created the app and add the ID of the application to the applications array
-  createApplication(req, res) {
-    Application.create(req.body)
-      .then((application) => {
-        return User.findOneAndUpdate(
-          { _id: req.body.userId },
-          { $addToSet: { applications: application._id } },
-          { new: true }
-        );
-      })
-      .then((user) =>
-        !user
-          ? res.status(404).json({
-              message: 'Application created, but found no user with that ID',
-            })
-          : res.json('Created the application 🎉')
-      )
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  },
-  // Updates and application using the findOneAndUpdate method. Uses the ID, and the $set operator in mongodb to inject the request body. Enforces validation.
-  updateApplication(req, res) {
-    Application.findOneAndUpdate(
-      { _id: req.params.applicationId },
-      { $set: req.body },
-      { runValidators: true, new: true }
-    )
-      .then((application) =>
-        !application
-          ? res.status(404).json({ message: 'No application with this id!' })
-          : res.json(application)
-      )
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  },
-  // Deletes an application from the database. Looks for an app by ID.
-  // Then if the app exists, we look for any users associated with the app based on he app ID and update the applications array for the User.
-  deleteApplication(req, res) {
-    Application.findOneAndRemove({ _id: req.params.applicationId })
-      .then((application) =>
-        !application
-          ? res.status(404).json({ message: 'No application with this id!' })
-          : User.findOneAndUpdate(
-              { applications: req.params.applicationId },
-              { $pull: { applications: req.params.applicationId } },
-              { new: true }
-            )
-      )
-      .then((user) =>
-        !user
-          ? res.status(404).json({
-              message: 'Application created but no user with this id!',
-            })
-          : res.json({ message: 'Application successfully deleted!' })
-      )
-      .catch((err) => res.status(500).json(err));
-  },
-  // Adds a tag to an application. This method is unique in that we add the entire body of the tag rather than the ID with the mongodb $addToSet operator.
-  addTag(req, res) {
-    Application.findOneAndUpdate(
-      { _id: req.params.applicationId },
-      { $addToSet: { tags: req.body } },
-      { runValidators: true, new: true }
-    )
-      .then((application) =>
-        !application
-          ? res.status(404).json({ message: 'No application with this id!' })
-          : res.json(application)
-      )
-      .catch((err) => res.status(500).json(err));
-  },
-  // Remove application tag. This method finds the application based on ID. It then updates the tags array associated with the app in question by removing it's tagId from the tags array.
-  removeTag(req, res) {
-    Application.findOneAndUpdate(
-      { _id: req.params.applicationId },
-      { $pull: { tags: { tagId: req.params.tagId } } },
-      { runValidators: true, new: true }
-    )
-      .then((application) =>
-        !application
-          ? res.status(404).json({ message: 'No application with this id!' })
-          : res.json(application)
-      )
-      .catch((err) => res.status(500).json(err));
-  },
+    },
+    deleteReaction(req, res) {
+        Thought.findOneAndUpdate({ _id: req.params.thoughtId }, { $pull: { reactions: { reactionId: req.params.reactionId } } }, { runValidators: true, new : true })
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({message: 'No reactions found'});
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.status(400).json(err));
+    }
 };
+
+
+
+module.exports = thoughtController; 
